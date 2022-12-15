e23ebb1a7d49b50d94218ba8b1c4df1bcd89ebf7
Atul Agrawal <atul60556@gmail.com>
2022-12-15 23:03:47 +0530

    added alerts react spnits nd validation

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,14 +1,54 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import logo from '../assets/logo.png'
 import { Link } from 'react-router-dom';
 import styled from "styled-components";
+import {ToastContainer , toast} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function Register() {
+  const [values, setValues] = useState({
+    username : "",
+    email : "",
+    password : "",
+    confrmpassword : ""
+  });
+  const toastOptions = {
+    position:"bottom-right",
+    autoClose : 8000,
+    pauseOnHover : true,
+    draggable : true,
+    theme : 'dark' ,
+  };
   const handelSubmit = (event) =>{
     event.preventDefault();
-    alert("form");
+   if(handelValidation()){
+    alert('call');
+   }
   }
+
+  const handelValidation = () =>{
+    const{username,email,password,confrmpassword} = values;
+    if(password !== confrmpassword){
+      toast.error("Password & Confirm Password should be same",toastOptions);
+      return false;
+    }
+    else if(username.length < 4 ){
+      toast.error("User Name should contain atleast 4 characters",toastOptions);
+      return false;
+    }
+    else if(email === '' ){
+      toast.error("Email should required",toastOptions);
+      return false;
+    }
+    else if(password.length < 8){
+      toast.error("Password should contain atleast 8 characters",toastOptions);
+      return false;
+    }
+    return true;
+  }
+
   const handelChange = (event) =>{
+    setValues({...values,[event.target.name]: event.target.value});
 
   }
   return (
@@ -22,11 +62,12 @@ function Register() {
           <input type="text" name="username" placeholder='Enter Your Name' onChange={e=>handelChange(e)}/>
           <input type="email" name="email" placeholder='Enter Your Email' onChange={e=>handelChange(e)}/>
           <input type="password" name="password" placeholder='Enter Your Password' onChange={e=>handelChange(e)}/>
-          <input type="password" name="Confrmpassword" placeholder='Enter Your Confirm Password' onChange={e=>handelChange(e)}/>
+          <input type="password" name="confrmpassword" placeholder='Enter Your Confirm Password' onChange={e=>handelChange(e)}/>
           <button type='submit'>Create User</button>
           <span>Already have a account ? <Link to="/login">Login </Link> </span>
         </form>
       </FormContainer>
+      <ToastContainer />
     </>
   )
 }
@@ -81,6 +122,22 @@ form{
     font-weight : bold;
     cursor : pointer;
     border-radius : 0.4rem;
+    font-size : 1rem;
+    text-transform : uppercase;
+    transition : 0.5s ease-in-out;
+    &:hover{
+      background-color : #4e0eff;
+    }
+  }
+  span{
+    color : white;
+    text-transform : uppercase;
+    a{
+      color : #4e0eff;
+      text-transform : none;
+      text-decoration : none;
+      font-weight : bold;
+    }
   }
 
 }
